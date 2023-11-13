a278d0e651330654c6ed76add0a90d9fd7455db0
Angel Aguero <angelagueror23@gmail.com>
2023-11-12 22:37:14 -0400

    LAYOUTS, SLOTS Y COMPOSABLES

---
+++ app-example-24/src/components/AuMenu.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <section>
+    <h2>Menu</h2>
+  </section>
+</template>
---
+++ app-example-24/src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AuMenu from './AuMenu.vue';
+import AuxBar from './AuxBar.vue';
+
+
+</script>
+<template>
+  <main class="container">
+    <div class="auxbar">
+    <AuxBar />
+    </div>
+    <div class="content">
+      <AuMenu />
+    </div>
+    <div>
+      <h2>My home content</h2>
+    </div>
+  </main>
+</template>
