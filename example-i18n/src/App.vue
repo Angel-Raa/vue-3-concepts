275cf9ff6ad038ed20de2856639b35920b53ce0b
Angel Aguero <angelagueror23@gmail.com>
2023-11-21 19:10:02 -0400

    i18n

---
+++ example-i18n/src/App.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import { ref, type Ref } from 'vue'
+
+const username: Ref<String> = ref('Angel')
+</script>
+<template>
+  <nav>
+    <select v-model="$i18n.locale">
+      <option v-for="locale in $i18n.availableLocales" :key="locale" :value="locale">{{ locale }}</option>
+    </select>
+  </nav>
+  <main>
+    <div>
+      <h1>{{ $t('msg.avatar', { name: username }) }}</h1>
+      <h2>{{ $t('msg.hello') }}</h2>
+      <p>{{ $t('msg.content') }}</p>
+    </div>
+  </main>
+</template>
+
+<style>
+body {
+  background: #000;
+  display: flex;
+  flex-wrap: nowrap;
+  width: auto;
+  height: auto;
+  justify-content: center;
+  color: #90f;
+}
+nav {
+}
+div {
+  border: 2px solid #09f;
+  border-radius: 12px;
+  box-sizing: border-box;
+  font-size: 16px;
+  opacity: 0.9;
+
+  text-align: center;
+}
+
+p {
+  margin: 12px;
+}
+</style>
---
+++ example-i18n/src/lang/messages.ts
@@ -0,0 +1,18 @@
+export const messages= {
+  en: {
+    msg: {
+      hello: 'Hello World',
+      content:
+        'In computing, Hello, world or Hello, World! in English is a program that displays the text Hello, world! on a display device, in most cases the monitor screen.',
+      avatar :'Welcome {name}'
+    }
+  },
+  es: {
+    msg: {
+      hello: 'Hola Mundo',
+      content:
+        ' En informática, Hola, mundo o Hello, World!, en inglés es un programa que muestra el texto «¡Hola, mundo!» en un dispositivo de visualización, en la mayoría de los casos la pantalla de un monitor.',
+      avatar:'Bienvenidos {name}'
+    }
+  }
+}
