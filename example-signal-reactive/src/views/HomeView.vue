8ff784f4ea1060d4bd182eedd4c41276644dc50a
Angel Aguero <angelagueror23@gmail.com>
2023-11-22 13:40:59 -0400

    Signal, Reactive

---
+++ example-signal-reactive/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { createSignal } from 'vue-signals/solid'
+import { signal, computed } from 'vue-signals/angular'
+const [count, setCount] = createSignal(0)
+const contado = signal(0)
+const doble = computed(() => contado() * 2)
+</script>
+
+<template>
+  <main>
+    <h1>Home Views</h1>
+
+    <p>Solid Count : {{ count() }}</p>
+    <button @click="setCount((v) => v + 1)">increment</button>
+    <section>
+      <h2>Angular Contado : {{ contado() }}</h2>
+      <h3>Doble: {{ doble() }}</h3>
+      <button @click="contado.update((v) => v + 1)">increment</button>
+      <button @click="contado.set(0)">Resert</button>
+    </section>
+  </main>
+</template>
+<style scoped>
+input {
+  display: block;
+}
+</style>
