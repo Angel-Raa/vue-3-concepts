5ca6480d1fe99f3f624face996e550f1a0cd2aa0
Angel Aguero <angelagueror23@gmail.com>
2023-11-23 13:08:10 -0400

    Home View

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -4,7 +4,9 @@ import ComputedBill from './components/app-bill/ComputedBill.vue'
 
 <template>
   <div class="container mx-auto">
-    <h1 class="text-justify text-gray-500 text-2xl">Home</h1>
+    <h1 class="text-justify text-gray-500 text-2xl">Home View</h1>
+    <h2 class="container"></h2>
     <ComputedBill />
   </div>
 </template>
+<style scoped></style>
