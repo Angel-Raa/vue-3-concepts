c4d928228c2867ad9793afbdda5a838c5e1a02d8
Angel Aguero <angelagueror23@gmail.com>
2023-12-04 17:49:38 -0400

    Testing unitario con Jest

--- unitario-test/src/components/ICounter.vue
+++ unitario-test/src/components/ICounter.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import {ref, type Ref} from 'vue';
-const counter:Ref<number> = ref(0);
-const increment = ():void =>{
-    counter.value ++;
+import { ref, type Ref } from 'vue'
+const counter: Ref<number> = ref(0)
+const increment = (): void => {
+  counter.value++
 }
 </script>
 <template>
-    <h3>Counter : {{  counter }}</h3>
-    <button @click="increment()">Increment</button>
+  <h3>{{ counter }}</h3>
+  <button @click="increment()">Increment</button>
 </template>
--- unitario-test/src/components/__tests__/counter.spec.ts
+++ unitario-test/src/components/__tests__/counter.spec.ts
@@ -1,17 +1,13 @@
 import { describe, it } from 'node:test'
 import ICounter from '../ICounter.vue'
 import { mount } from '@vue/test-utils'
+import { expect } from '@jest/globals'
 
 describe('My test counter ', () => {
   it('The component render the initial state', () => {
     const wrapper = mount(ICounter)
-    if(expect( wrapper.find('h3').exists())){
-        expect(wrapper.text()).toBe('Counter : 0')
+    if (expect(wrapper.find('h3').exists())) {
+      expect(wrapper.text()).toBe(' 0')
     }
-    
-
-  })
-  it('The increment btn add ones to counter', () => {
-    //  TODO
   })
 })
