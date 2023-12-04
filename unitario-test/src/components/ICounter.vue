7fd0100dde7545b93a5dcc93c89fc1187c31a5fd
Angel Aguero <angelagueror23@gmail.com>
2023-12-04 17:36:20 -0400

    Testing unitario con Jest

---
+++ unitario-test/src/components/ICounter.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ref, type Ref} from 'vue';
+const counter:Ref<number> = ref(0);
+const increment = ():void =>{
+    counter.value ++;
+}
+</script>
+<template>
+    <h3>Counter : {{  counter }}</h3>
+    <button @click="increment()">Increment</button>
+</template>
---
+++ unitario-test/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ICounter from '@/components/ICounter.vue';
+
+</script>
+
+<template>
+  <main>
+   <h2>Hello World!</h2>
+   <ICounter />
+  </main>
+</template>
