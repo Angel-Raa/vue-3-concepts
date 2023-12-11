8681383707532741e48a433ae1272e18b2575b8e
Angel Aguero <angelagueror23@gmail.com>
2023-12-11 13:00:17 -0400

    Deploy

--- app-deploy/src/views/HomeView.vue
+++ app-deploy/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import {ref, type Ref} from "vue";
+const counter:Ref<number> = ref(0);
+const increment = () => {
+  counter.value ++
+}
 
 </script>
 
 <template>
   <main>
    <h1>Home Views </h1>
+   <button class="btn" @click="increment()">increment</button>
+   <p>{{ counter }}</p> 
   </main>
 </template>
+
+<style scoped>
+.btn {
+  width: 100px;
+  height: 30px;
+  background-color: #09f;
+  border-radius: 12px;
+  border: thin solid #000;
+}
+
+</style>
