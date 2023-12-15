a662ed1a1e543627d71514a23fd4d4e28db70bab
Angel Aguero <angelagueror23@gmail.com>
2023-12-14 21:53:09 -0400

    Nuxt

--- project-one/app.vue
+++ project-one/app.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import Header from "./components/Header.vue"
+
 </script>
 <template>
   <div>
     <NuxtLayout>
-    <Header />
       <NuxtPage />
     </NuxtLayout>
   </div>
--- project-one/layouts/default.vue
+++ project-one/layouts/default.vue
@@ -5,6 +5,7 @@
         <li><NuxtLink to="/"> Home </NuxtLink></li>
         <li><NuxtLink to="/about"> About </NuxtLink></li>
         <li><NuxtLink to="/post/"> Post </NuxtLink></li>
+        <li><NuxtLink to="/service"> Service </NuxtLink></li>
       </ul>
     </nav>
     <slot />
