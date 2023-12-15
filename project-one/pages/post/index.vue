6e3f1ad0ac4f71f41e082b412062a585b05e1217
Angel Aguero <angelagueror23@gmail.com>
2023-12-15 19:55:44 -0400

    FULLSTACK con MongoDB / Mongoose

--- project-one/pages/post/index.vue
+++ project-one/pages/post/index.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { getNotes } from "~/service";
+const notes = ref();
+onMounted(() => {
+    loading()
+})
+
+const loading = async () => {
+    notes.value = await getNotes()
+}
+</script>
+
 <template>
-    <h2>Posts</h2>
+  <h2>Posts</h2>
+  <div>
+    <h1>Title : {{ notes.title }}</h1>
+    <p>{{ notes.content }}</p>
+  </div>
 </template>
---
+++ project-one/server/api/notes/index.get.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+  return {
+    id: 1,
+    name: "John Doe",
+    email: "john.@gmail.com",
+    title: "Que en Java",
+    content: "lorem ipsum dolor set aimer",
+  };
+});
